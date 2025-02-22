8a05e38537c0c8c2b415bd0809489be8fca6aaa2
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2025-02-22 15:10:34 +0530

    layers and dot product

---
+++ neural net from scratch/dot_product.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+inputs= [1, 2, 3, 2.5]
+weights =[
+    [0.2, 0.8, -0.5, 1],
+    [0.2, 0.8, -0.5, 1],
+    [0.2, 0.8, -0.5, 1]
+]
+
+bias = [1, 2, 3]
+ 
+# layer_output = []
+# for neuron_weight, neuron_bias in zip(weights, bias):
+#     neuron_output = 0
+#     for n_input, weight in zip(inputs, neuron_weight):
+#         neuron_output += n_input * weight
+#     neuron_output += neuron_bias
+
+#     layer_output.append(neuron_output)
+
+# print(layer_output)
+# inputs= [1, 2, 3, 2.5]
+# weights = [0.2, 0.8, -0.5, 1.0]
+# bias= 2.0
+
+output = np.dot(weights, inputs) + bias 
+print(output )
+
+
+
+
+
+
--- neural net from scratch/neuron_and_layer_from_scrathch.py
+++ neural net from scratch/neuron_and_layer_from_scrathch.py
@@ -1,22 +1,25 @@
 import numpy as np
 
-input1 = [1.5, 2.5, 1, 1]
-weights1 = [0.2, 0.8, -0.5, 1]
-input2 = [1.5, 2.5, 1, 1]
-weights2 = [0.2, 0.8, -0.5, 1]
-input3 = [1.5, 2.5, 1, 1]
-weights3 = [0.2, 0.8, -0.5, 1]
+inputs= [1, 2, 3, 2.5]
+weights =[
+    [0.2, 0.8, -0.5, 1],
+    [0.2, 0.8, -0.5, 1],
+    [0.2, 0.8, -0.5, 1]
+]
 
-bias1= 1
-bias2= 2
-bias3= 3
+bias = [1, 2, 3]
  
-otuput = [0, 0, 0]
-otuput[0] = input1[0]*weights1[0] + input1[1]*weights1[1] + input1[2]*weights1[2] + input1[3]*weights1[3] + bias1
-otuput[1] = input2[0]*weights2[0] + input2[2]*weights2[1] + input2[2]*weights2[2] + input2[3]*weights2[3] + bias2
-otuput[2] = input3[0]*weights3[0] + input3[1]*weights3[1] + input3[2]*weights3[2] + input3[3]*weights3[3] + bias3
+layer_output = []
+for neuron_weight, neuron_bias in zip(weights, bias):
+    neuron_output = 0
+    for n_input, weight in zip(inputs, neuron_weight):
+        neuron_output += n_input * weight
+    neuron_output += neuron_bias
 
-print(otuput[0])
-print(otuput[1])
-print(otuput[2])
+    layer_output.append(neuron_output)
+
+print(layer_output)
+
+
+
 
