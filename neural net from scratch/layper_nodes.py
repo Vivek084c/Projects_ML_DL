3f13f51d269c6808825fb56d349d202219c6bd51
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2025-02-25 19:47:45 +0530

    custom neural net

--- neural net from scratch/layper_nodes.py
+++ neural net from scratch/layper_nodes.py
@@ -34,6 +34,25 @@ class Activation_Softmax:
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
 
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss 
+    
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred = np.clip(y_pred, 1e-7, 1-1e-7)  
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred*y_true, axis=1) 
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
 
 X, y = spiral_data(samples = 100, classes= 3)
 
@@ -51,6 +70,9 @@ activation_1.forward(dense1.output)
 dense2.forward(activation_1.output)
 activation_2.forward(dense2.output)
 
-print(activation_2.output)
+print(activation_2.output[:5])
+
+loss_function = Loss_CategoricalCrossentropy()
+loss_function.calculate(activation_2.output, y)
 
 
